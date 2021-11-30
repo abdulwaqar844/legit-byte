3ecf32670c8c819c853ff5a9f6b44fa0a343e750
abdulwaqar844 <abdulwaqar844@gmail.com>
2021-12-01 02:12:40 +0500

    main page updated

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,37 +4,86 @@ import { Link } from "gatsby"
 import { useEffect } from "react"
 
 const isActive = ({ isCurrent }) => {
-  return isCurrent ? { className: "nav-link active" } : {className: "nav-link"}
+  return isCurrent
+    ? { className: "nav-link active" }
+    : { className: "nav-link" }
 }
 
-const ExactNavLink = props => (
-  <Link getProps={isActive} {...props} />
-)
+const ExactNavLink = props => <Link getProps={isActive} {...props} />
 
 const Navbar = ({ siteTitle }) => {
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-primary">
-      <div className="container-fluid">
-        <Link to="/" className="navbar-brand" href="#">{siteTitle}</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-navbar"
-                aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
+    <nav className="navbar navbar-expand-md navbar-light bg-secondary pt-4">
+      <div className="container-fluid align-items-center">
+        <Link to="/" className="navbar-brand mb-2" href="#">
+          <svg
+            width="30"
+            height="30"
+            viewBox="0 0 52 53"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M34.4095 52H13.5286C13.5286 52 1 52 1 39.5C1 27 1 39.5 1 39.5V2L9.1088 8.5611L9.35236 39.5C9.35236 39.5 9.32692 43.635 13.5031 43.635C17.6793 43.635 17.7047 43.6667 17.7047 43.6667H34.4095V52Z"
+              fill="#D40000"
+              stroke="#FF6600"
+              stroke-width="0.999999"
+            />
+            <path
+              d="M17.8718 35.3333V2L25.9806 7.83209L26.2242 20.9425"
+              fill="#2F3640"
+            />
+            <path
+              d="M17.8718 35.3333V2L25.9806 7.83209L26.2242 20.9425"
+              stroke="#2F3640"
+              stroke-width="0.999999"
+            />
+            <path
+              d="M34.4142 51.8815C43.5743 51.8815 51.0001 44.4726 51.0001 35.3333L34.4142 51.8815ZM51.0001 35.3333C51.0001 26.1941 43.5743 18.7852 34.4142 18.7852C25.254 18.7852 17.8282 26.1941 17.8282 35.3333C17.8282 44.4726 25.254 51.8815 34.4142 51.8815L51.0001 35.3333ZM42.6226 35.2708C42.6226 39.8371 38.9125 43.5387 34.3359 43.5388L42.6226 35.2708ZM34.3359 43.5388C29.7591 43.5389 26.0488 39.8372 26.0487 35.2708C26.0486 30.7044 29.759 27.0025 34.3359 27.0026C38.9126 27.0027 42.6228 30.7045 42.6226 35.2708"
+              fill="#2F3640"
+            />
+            <path
+              d="M34.3359 43.5388C29.7591 43.5389 26.0488 39.8372 26.0487 35.2708C26.0486 30.7044 29.759 27.0025 34.3359 27.0026C38.9126 27.0027 42.6228 30.7045 42.6226 35.2708M34.4142 51.8815C43.5743 51.8815 51.0001 44.4726 51.0001 35.3333L34.4142 51.8815ZM51.0001 35.3333C51.0001 26.1941 43.5743 18.7852 34.4142 18.7852C25.254 18.7852 17.8282 26.1941 17.8282 35.3333C17.8282 44.4726 25.254 51.8815 34.4142 51.8815L51.0001 35.3333ZM42.6226 35.2708C42.6226 39.8371 38.9125 43.5387 34.3359 43.5388L42.6226 35.2708Z"
+              stroke="#2F3640"
+              stroke-width="1.13474"
+            />
+            <path
+              d="M34.562 47.7465V43.7172L35.3407 43.634C36.4115 43.5195 37.9647 42.9602 38.8999 42.3523C39.8075 41.7624 40.9522 40.6024 41.5383 39.6786C42.0641 38.85 42.6138 37.2655 42.7206 36.2709L42.804 35.494H46.8495H50.895L50.8084 36.4419C50.5319 39.4688 49.6201 42.088 47.9874 44.5448C47.2033 45.7249 44.8162 48.1064 43.6335 48.8888C41.1514 50.5308 38.53 51.4309 35.4508 51.6986L34.562 51.7758V47.7465Z"
+              fill="#D40000"
+              stroke="#FF6600"
+              stroke-width="0.733852"
+            />
+          </svg>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#main-navbar"
+          aria-controls="main-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="main-navbar">
-          <ul className="navbar-nav me-auto mb-2 mb-md-0">
+        <div className="collapse navbar-collapse " id="main-navbar">
+          <ul className="navbar-nav ms-auto mb-2 mb-md-0 align-items-center">
             <li className="nav-item">
-              <ExactNavLink
-                to="/"
-              >
-                Home
-              </ExactNavLink>
+              <ExactNavLink to="/services">SERVICES</ExactNavLink>
             </li>
             <li className="nav-item">
-              <ExactNavLink
-                to="/about"
-              >
-                About
+              <ExactNavLink to="/case-studies">CASE STUDIES</ExactNavLink>
+            </li>{" "}
+            <li className="nav-item">
+              <ExactNavLink to="/careers">CAREERS</ExactNavLink>
+            </li>{" "}
+            <li className="nav-item">
+              <ExactNavLink to="/about">BLOG</ExactNavLink>
+            </li>{" "}
+            <li className="nav-item ">
+              <ExactNavLink className="p-0 " to="/get-start">
+                <p className="btn btn-outline-primary "> LET’S TALK</p>
               </ExactNavLink>
             </li>
           </ul>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,163 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-
+import HeroImage from "./../images/hero.svg"
+import Mobile from "./../images/mobile.svg"
+import Consult from "./../images/consult.svg"
+import Server from "./../images/server.svg"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <section className="py-5 text-center container">
-      <div className="row py-lg-5">
-        <div className="col-lg-6 col-md-8 mx-auto">
-          <h1 className="fw-light">Hello world ! </h1>
-          <p className="lead text-muted"> Welcome to this Boostrap 5 Gatsby Starter</p>
-          <StaticImage
-            src="../images/gatsby-astronaut.png"
-            width={300}
-            quality={95}
-            formats={["AUTO", "WEBP"]}
-            alt="A Gatsby astronaut"
-            className="img-fluid"
-          />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 col-sm-12">
+          <div className="jumbotron ">
+            <h1 className="display-6 text-primary pt-3 pb-3">
+              Delivering Innovative <span className="text-dark"> and</span> Cost
+              Effective Digital Solutions
+            </h1>
+            <p className="lead pt-3 pb-3">
+              Lorem ipsum dolor sit amet, consectetur{" "}
+              <span className="text-primary"> adipiscing elit</span> . Phasellus
+              quam lectus malesuada venenatis ut{" "}
+              <span className="text-primary"> nulla</span> commodo. Nunc nulla.
+            </p>
 
+            <p className="lead pt-3 pb-3">
+              <a className="btn btn-primary btn-lg" href="#" role="button">
+                Learn more
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className=" col-md-6 col-sm-12">
+          <div className="jumbotron align-items-end ">
+            <img
+              width={670}
+              height={580}
+              className="heroimage "
+              src={HeroImage}
+              alt="Legit byte"
+            />
+          </div>
         </div>
       </div>
-      <div className="row">
-        <Link to="/about/" className="btn btn-primary my-2">About</Link>
-        <Link to="/page-2/" className="btn btn-secondary my-2">Go to page 2</Link>
+      <div className="row pt-2 mt-2 ">
+        <p className="h2 text-center pt-3">
+          Awesome Services For <span className="text-primary">Customer</span>
+        </p>{" "}
       </div>
-    </section>
+      <div className="row shadow   pt-4 pb-4 mt-2 mb-2">
+        <div className="col-md-4">
+          <img src={Mobile} alt="Mobile" height={260} />
+        </div>
+        <div className="col-md-8  d-flex ">
+          <div className="body  ">
+            <h5 className="title pt-4">Rapid MVP Development</h5>
+            <p className="text pt-4">
+              We can help you design and develop a high-quality Minimum Viable
+              Product to validate your business assumptions faster and more
+              efficiently. Our goal is creating successful products that your
+              users will love.
+            </p>
+            <a href="#" className="pt-3  text-primary">
+              Get Started
+            </a>
+          </div>
+        </div>
+      </div>{" "}
+      <div className="row shadow  pt-4 pb-4 mt-2 mb-2">
+        <div className="col-md-4">
+          <img src={Server} alt="Mobile" height={260} />
+        </div>
+        <div className="col-md-8  d-flex ">
+          <div className="body  ">
+            <h5 className="title pt-4">Devops Implementation</h5>
+            <p className="text pt-4">
+              A transformational shift which incorporates secure culture,
+              practices, and tools to drive visibility, collaboration, and
+              agility of security into each phase of the DevOps pipeline
+            </p>
+            <a href="#" className="pt-3  text-primary">
+              Get Started
+            </a>
+          </div>
+        </div>
+      </div>{" "}
+      <div className="row shadow  pt-4 pb-4 mt-2 mb-2">
+        <div className="col-md-4">
+          <img src={Consult} alt="Mobile" height={260} />
+        </div>
+        <div className="col-md-8  d-flex ">
+          <div className="body  ">
+            <h5 className="title pt-4">Cloud Consulting</h5>
+            <p className="text pt-4">
+              With our team of certified cloud professional, unleash the full
+              potential of cloud and build more flexible, scalable and available
+              services.
+            </p>
+            <a href="#" className="pt-3  text-primary">
+              Get Started
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="container-sm">
+        {" "}
+        <div className="row  pt-4 mt-3">
+          <div className="col-md-3 border bg-primary text-light pt-2 d-flex flex-column justify-content-center align-items-center">
+            <h2>100%</h2>
+            <p>Payments Success</p>
+          </div>
+          <div className="col-md-3 border bg-primary text-light pt-2 d-flex flex-column justify-content-center align-items-center">
+            <h2>2000+</h2>
+            <p>Completed Project</p>
+          </div>{" "}
+          <div className="col-md-3 border bg-primary text-light pt-2 d-flex flex-column justify-content-center align-items-center">
+            <h2>10K</h2>
+            <p>Global Customer</p>
+          </div>{" "}
+          <div className="col-md-3 border bg-primary text-light pt-2 d-flex flex-column justify-content-center align-items-center">
+            <h2>24/7</h2>
+            <p>Always-on For Support</p>
+          </div>
+        </div>
+      </div>
+      <div className="container-sm">
+        {" "}
+        <div className="row  pt-4 mt-3">
+          <h2>Latest News From Blog</h2>
+        </div>
+        <div className="col-md-6">
+          <p className="bg-light h4">Technology</p>
+          <p className="title">Facebook is changing its name to Meta</p>
+          <p>24 November 2021</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
+            mauris porta ultrices non massa vitae dignissim nunc. Amet netus
+            imperdiet sed sit leo, consectetur vitae, cursus.
+          </p>
+        </div>
+        <div className="col-md-6">
+          <div className="row">
+            <h3>Popular Post</h3>
+          </div>
+          <div className="row">
+            <div className='col'>   <p className="bg-light h4">Technology</p>
+          <p className="title">Facebook is changing its name to Meta</p>
+          <p>24 November 2021</p></div><div className='col'>   <p className="bg-light h4">Technology</p>
+          <p className="title">Facebook is changing its name to Meta</p>
+          <p>24 November 2021</p></div><div className='col'>   <p className="bg-light h4">Technology</p>
+          <p className="title">Facebook is changing its name to Meta</p>
+          <p>24 November 2021</p></div><div className='col'>   <p className="bg-light h4">Technology</p>
+          <p className="title">Facebook is changing its name to Meta</p>
+          <p>24 November 2021</p></div>
+          </div>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
