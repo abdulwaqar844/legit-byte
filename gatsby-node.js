57ee393041ffe50451ed0ec44c7ae7c5e60778c0
Abdul Waqar <63345475+abdulwaqar844@users.noreply.github.com>
2021-12-03 15:39:53 +0500

    updated nodejs config file

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,44 @@
  */
 
 // You can delete this file if you're not using it
+const path = require("path")
+
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  // Query for markdown nodes to use in creating pages.
+  const result = await graphql(
+    `
+      {
+        allWpPost {
+          nodes {
+            slug
+          }
+        }
+      }
+    `
+  )
+  console.log(result)
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  // Create pages for each markdown file.
+  const blogPostTemplate = path.resolve(`src/templates/post.js`)
+  result.data.allWpPost.nodes.map((post) => {
+    const path = post.slug
+    createPage({
+      path,
+      component: blogPostTemplate,
+      // In your blog post template's graphql query, you can use pagePath
+      // as a GraphQL variable to query for data from the markdown file.
+      context: {
+        pagePath: path,
+      },
+    })
+  })
+}
